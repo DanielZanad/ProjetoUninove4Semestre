0ece1f2c651bca022132d1fdcf79e0ebec660cf1
Zanad <daniel.zanad.dz@gmail.com>
2021-09-28 15:34:08 -0300

    Adicionando base do projeto, mais a criacao do header e nav, precisa arrumar parte do 'entre ou Cadastre' e terminar nav

---
+++ header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="./styles/style.css">
+<header class="menu-principal">
+        <img src="./img/WebLivrariaLogo_1.png" id="logo"/>
+        <input type="search" placeholder="search" id="search-box">
+        <div class="login">
+            <img src="./img/carbon_user-filled.png" />
+            <a href="">Entre</a>
+            ou
+            <a href="">Cadastrase-se</a>
+            <a href="">
+                <img src="./img/clarity_shopping-cart-solid.png" />
+            </a>
+        </div>
+</header>
---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <select name="Categorias" id="categoria">
+        <option value="">teste</option>
+        <option value="">teste</option>
+        <option value="">teste</option>
+    </select>
+    <div id="links">
+        <a href="#">Livros</a>
+        <a href="#">Jogos</a>
+        <a href="#">Brinquedos</a>
+        <a href="#">Filmes e Series</a>
+    </div>
+    
+</nav>
