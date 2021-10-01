f8ee08da55ddc47ac5f00005e510b9611a1dcbf4
Zanad <daniel.zanad.dz@gmail.com>
2021-10-01 09:31:18 -0300

    path img corrigido

--- src/header.php
+++ src/header.php
@@ -1,15 +1,15 @@
 <link rel="stylesheet" href="./styles/style.css">
 <header class="menu-principal">
-        <img src="./img/WebLivrariaLogo_1.png" id="logo"/>
+        <img src="../img/WebLivrariaLogo_1.png" id="logo"/>
         <input type="search" placeholder="search" id="search-box">
         <p>Olá, Seja Bem vindo!</p>
         <div class="login">
-            <img src="./img/carbon_user-filled.png" />
+            <img src="../img/carbon_user-filled.png" />
             <a href="">Entre</a>
             <p>Ou</p>
             <a href="">Cadastrase-se</a>
             <a href="">
-                <img src="./img/clarity_shopping-cart-solid.png" id="carrinho" />
+                <img src="../img/clarity_shopping-cart-solid.png" id="carrinho" />
             </a>
         </div>
 </header>
