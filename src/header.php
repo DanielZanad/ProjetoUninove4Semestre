17e65ec268109a97f01ac7f7e442954f59c66636
João Vitor <joaoquickgamer@hotmail.com>
2021-10-01 10:17:17 -0300

    criação da barra todo site
    Criação da barra Todo Site, necessita de reposicionar e arrumar a fonte e cor.

--- src/header.php
+++ src/header.php
@@ -1,15 +1,21 @@
 <link rel="stylesheet" href="./styles/style.css">
 <header class="menu-principal">
-        <img src="../img/WebLivrariaLogo_1.png" id="logo"/>
-        <input type="search" placeholder="search" id="search-box">
+        <img src="./img/WebLivrariaLogo_1.png" id="logo"/>
+        <input type="search" placeholder="" id="search-box">
+        <select name="Todo o site" id="todosite">
+        <option value="">Livros</option>
+        <option value="">Jogos</option>
+        <option value="">Brinquedos</option>
+        <option value="">Filmes e Series</option>
+        </select>
         <p>Olá, Seja Bem vindo!</p>
         <div class="login">
-            <img src="../img/carbon_user-filled.png" />
+            <img src="./img/carbon_user-filled.png" />
             <a href="">Entre</a>
             <p>Ou</p>
             <a href="">Cadastrase-se</a>
             <a href="">
-                <img src="../img/clarity_shopping-cart-solid.png" id="carrinho" />
+                <img src="./img/clarity_shopping-cart-solid.png" id="carrinho" />
             </a>
         </div>
 </header>
