734ade39879c6ab3a877b874102d1b0c6666f771
DanielZanad <64167362+DanielZanad@users.noreply.github.com>
2021-11-23 15:50:45 -0300

    Update Conexao.php

--- app/model/Conexao.php
+++ app/model/Conexao.php
@@ -10,7 +10,7 @@ class Conexao{
     public static function getConn(){
         if(!isset(self::$instance)){
             // Criando uma instancia nova   
-            self::$instance = new PDO('mysql:host=mysqlserver.cmale3pklhsm.us-east-1.rds.amazonaws.com;dbname=usuario;charset=utf8','admin','TwMPMqtFk#Uoo');
+            self::$instance = new PDO('mysql:host=mysqlserver.cm.us-east-1.rds.amazonaws.com;dbname=usuario;charset=utf8','admin','');
         }
         // Retornando a instancia
         return self::$instance;
